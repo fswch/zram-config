617926c615f495e3240eecaf11dd8ac8dd8fc9fb
Claude Nobs <claudenobs@blunet.cc>
2021-02-15 21:41:38 +0100

    installer: reset packages to their initial state

--- install.bash
+++ install.bash
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+package_status() {
+  dpkg-query --show --showformat='${db:Status-Abbrev}\n' "$1"
+}
+require_package() {
+  STATUS=$( package_status "$1" )
+  if [[ "$STATUS" -eq 'un' ]]; then
+    if [[ ! "$STATUS" -eq 'ii' ]]; then
+      echo "ERROR: package $1 is in unclean state."
+      exit 1
+    fi
+    apt install --yes "$1" || exit 1
+  fi
+}
+list_packages() {
+  dpkg-query --show --showformat='${Package} '
+}
+
 if [[ "$(id -u)" -ne 0 ]]; then
   echo "ERROR: You need to be ROOT (sudo can be used)."
   exit 1
@@ -13,14 +30,21 @@ if [[ -f /usr/local/sbin/zram-config ]]; then
   exit 1
 fi
 
-echo "Installing needed packages (make, libattr1-dev)"
-if ! dpkg -s 'make' 'libattr1-dev' &> /dev/null; then
-  apt-get install --yes make libattr1-dev || exit 1
-fi
+echo "Installing needed packages (gcc, make, libattr1-dev)"
+PKGS_START=$( list_packages )
+require_package "gcc"
+require_package "make"
+require_package "libattr1-dev"
+PKGS_END=$( list_packages )
+PKGS_DIFF=$( echo $PKGS_START $PKGS_END | tr ' ' '\n' | sort | uniq -u | tr '\n' ' ' )
 
+echo "Building overlayfs-tools"
 cd overlayfs-tools || exit 1
 make
 cd ..
+
+echo "Removing temporarily installed packages"
+[[ ! -z "$PKGS_DIFF" ]] && apt remove --purge --yes $PKGS_DIFF
 
 echo "Installing zram-config files"
 install -m 755 zram-config /usr/local/sbin/
